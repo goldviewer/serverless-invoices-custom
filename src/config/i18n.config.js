89a04309c53e2db47137c10ffed319c477a5d695
yisrael <yisrael8544@gmail.com>
2022-04-18 18:50:42 -0400

    second

--- src/config/i18n.config.js
+++ src/config/i18n.config.js
@@ -9,26 +9,26 @@ import config from '@/config/app.config';
 Vue.use(VueI18Next);
 
 i18next
-  .use(LanguageDetector)
-  .use(Backend);
+    .use(LanguageDetector)
+    .use(Backend);
 
 const initialized = i18next.init({
-  fallbackLng: 'en',
-  whitelist: ['en', 'de', 'fr', 'et', 'fa', 'bn', 'es', 'pt_br'],
-  backend: {
-    loadPath: `${config.base_url || ''}/locales/{{lng}}/{{ns}}.json`,
-  },
-  detection: {
-    order: ['querystring', 'path', 'localStorage', 'navigator'],
-    lookupQuerystring: 'lang',
-    caches: ['localStorage'],
-    checkWhitelist: true,
-  },
+    fallbackLng: 'en',
+    whitelist: ['en'],
+    backend: {
+        loadPath: `${config.base_url || ''}/locales/{{lng}}/{{ns}}.json`,
+    },
+    detection: {
+        order: ['querystring', 'path', 'localStorage', 'navigator'],
+        lookupQuerystring: 'lang',
+        caches: ['localStorage'],
+        checkWhitelist: true,
+    },
 });
 initialized.then(() => app.$store.dispatch('language/initLanguage', i18next.language));
 
 const i18n = new VueI18Next(i18next, {
-  // loadComponentNamespace: true,
+    // loadComponentNamespace: true,
 });
 
 i18n.initialized = initialized;
